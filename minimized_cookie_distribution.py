f5bf6021258dfc6d48fca43259e9e3efc21a667c
sarath-jiguru <sarath@adnear.com>
2015-05-21 23:54:28 +0530

    working perfectly on various outputs

--- minimized_cookie_distribution.py
+++ minimized_cookie_distribution.py
@@ -2,11 +2,32 @@ from __builtin__ import xrange
 
 __author__ = 'sarath'
 
+
+# check boundaries as others will be good.
+def check_chocolates():
+    align_bool = True
+    for boundary in range(len(incr_boundaries)):
+        next2 = incr_boundaries[boundary] + 1
+        if student_array[incr_boundaries[boundary]] > student_array[next2]:
+            cookie_balance = chocolate_array[incr_boundaries[boundary]] > chocolate_array[next2]
+            print(student_array[incr_boundaries[boundary]], student_array[next2],
+                  chocolate_array[incr_boundaries[boundary]], chocolate_array[next2],
+                  cookie_balance)
+            align_bool = align_bool and cookie_balance
+    print("done", align_bool)
+    return align_bool
+
+
+def align_cookies():
+    for i in range(len(incr_boundaries)):
+        next1 = incr_boundaries[i] + 1
+        if student_array[incr_boundaries[i]] > student_array[next1] and not (
+                chocolate_array[incr_boundaries[i]] > chocolate_array[next1]):
+            chocolate_array[incr_boundaries[i]] += 1
+
+
 # Finding all possible non decreasing marks/scores
-
-
 def non_increasing_sequence(start_off=0):
-    print(start_off)
     index_initial = start_off
     index_final = start_off
 
@@ -16,25 +37,43 @@ def non_increasing_sequence(start_off=0):
             index_final = next1
         else:
             break
+    return index_initial, index_final + 1
 
-    print(index_initial, index_final+1)
-    for i in range(index_initial, index_final+1):
-        print(str(student_array[i]) + ", ")
-    if index_initial == index_final:
-        print("no more increasing arrays")
-    return index_final+1
 
 student_array = [6, 5, 4, 3, 2, 1]
 student_array = [1, 2, 3, 4, 5, 6]
 student_array = [4, 1, 2, 3, 1, 4, 9, 11]
 student_array = [15, 20, 5, 21, 50, 20, 11, 6, 10, 19, 11, 8, 9, 12]
-student_array = [1, 2, 3, 7, 6, 5, 4]
-
+# student_array = [1, 2, 3, 7, 6, 5, 4]
+student_array = [5, 10, 15, 11, 9, 8, 13, 25, 40, 50, 35, 12, 7]
 # Initialize Cookie array with all zeros.
 chocolate_array = [0] * len(student_array)
-
+incr_boundaries = []
 n = 0
 while n < len(student_array):
-    last_index = non_increasing_sequence(n)
+    scores_range = non_increasing_sequence(n)
+    size = scores_range[1] - scores_range[0]
+    incr_boundaries.append(scores_range[1] - 1)
+    min_cookie = 1
+    for cookie_index in range(scores_range[0], scores_range[1]):
+        print(cookie_index)
+        chocolate_array[cookie_index] = min_cookie
+        min_cookie += 1
+
+    last_index = non_increasing_sequence(n)[1]
     n = last_index
     print("next")
+
+# deleting the last element from the boundary check
+
+incr_boundaries.pop()
+
+print(student_array)
+print(chocolate_array)
+print(incr_boundaries)
+
+while check_chocolates() is False:
+    align_cookies()
+    print(chocolate_array)
+
+exit()
