1d33befb871f478c9a8e6d22bfe9a5c53fd5abd4
sarath-jiguru <sarath@adnear.com>
2015-05-21 20:44:01 +0530

    working non decreasing splits

--- minimized_cookie_distribution.py
+++ minimized_cookie_distribution.py
@@ -1,38 +1,40 @@
+from __builtin__ import xrange
+
 __author__ = 'sarath'
-
-# student_array = [4, 1, 2, 3, 1, 4, 9, 11]
-student_array = [6, 5, 4, 3, 2, 1]
-
-# Initialize Cookie array with all zeros.
-chocolate_array = [0] * len(student_array)
 
 # Finding all possible non decreasing marks/scores
 
 
 def non_increasing_sequence(start_off=0):
     print(start_off)
-    index_initial = 0
-    index_final = 0
-    for i in range(start_off, len(student_array) - 1):
-        next1 = i + 1
-        if student_array[i] > student_array[next1]:
-            index_initial = next1
-            break
+    index_initial = start_off
+    index_final = start_off
+
     for i in range(index_initial, len(student_array) - 1):
         next1 = i + 1
-        if student_array[i] > student_array[next1]:
+        if student_array[i] < student_array[next1]:
             index_final = next1
+        else:
             break
-        if next1 == len(student_array) - 1:
-            index_final = next1 + 1
-    print(index_initial, index_final)
-    for i in range(index_initial, index_final):
+
+    print(index_initial, index_final+1)
+    for i in range(index_initial, index_final+1):
         print(str(student_array[i]) + ", ")
     if index_initial == index_final:
         print("no more increasing arrays")
-    return index_final
+    return index_final+1
 
+student_array = [6, 5, 4, 3, 2, 1]
+student_array = [1, 2, 3, 4, 5, 6]
+student_array = [4, 1, 2, 3, 1, 4, 9, 11]
+student_array = [15, 20, 5, 21, 50, 20, 11, 6, 10, 19, 11, 8, 9, 12]
+student_array = [1, 2, 3, 7, 6, 5, 4]
 
-sequence = non_increasing_sequence()
-print("next")
-non_increasing_sequence(sequence - 1)
+# Initialize Cookie array with all zeros.
+chocolate_array = [0] * len(student_array)
+
+n = 0
+while n < len(student_array):
+    last_index = non_increasing_sequence(n)
+    n = last_index
+    print("next")
